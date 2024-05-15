629bfe1108eadcedefe9fd8f51de643220b597d7
narek.hovhannisyan-at-458685171505336 <narek.hovhannisyan@corel.com>
2024-05-15 14:36:07 +0400

    Updated cron

--- cron_main.cpp
+++ cron_main.cpp
@@ -8,6 +8,7 @@ struct DBCar
 {
     int Id;
     long int OnTopTs;
+    long int IsUrgentTs;
 };
 
 struct DBUser
@@ -58,21 +59,120 @@ void _ParseUsers(PGresult* res, std::vector<DBUser*>& users)
 
 int main()
 {
-    std::string sql = "SELECT id, on_top_ts FROM cars WHERE on_top > 0 AND on_top_ts > 0;";
-    PGconn* pConn = GetPQConnection();
-    PGresult* res = PQexec(pConn, sql.c_str());
-    if (PQresultStatus(res) != PGRES_TUPLES_OK)
-    {
-        fprintf(stderr, "SELECT failed: %s", PQerrorMessage(pConn));
-        PQclear(res);
-        return -1;
-    }
-
-    std::vector<DBCar*> cars;
-    _ParseCars(res, cars);
+    // On TOP
+    // std::string sql = "SELECT id, on_top_ts FROM cars WHERE on_top > 0 AND on_top_ts > 0;";
+    // PGconn* pConn = ConnectionPool::Get()->getConnection();
+    // PGresult* res = PQexec(pConn, sql.c_str());
+    // if (PQresultStatus(res) != PGRES_TUPLES_OK)
+    // {
+    //     fprintf(stderr, "SELECT failed: %s", PQerrorMessage(pConn));
+    //     PQclear(res);
+    //     return -1;
+    // }
 
     using namespace std::chrono;
-    uint64_t ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    uint64_t current_time_ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    PGconn* pConn = ConnectionPool::Get()->getConnection();
+
+    // Is urgent
+    {
+        std::string sql = "SELECT id, is_urgent_ts FROM cars WHERE is_urgent > 0 AND is_urgent_ts > 0;";
+        PGresult* res = PQexec(pConn, sql.c_str());
+
+        if (PQresultStatus(res) != PGRES_TUPLES_OK) 
+        {
+            fprintf(stderr, "SELECT failed: %s", PQerrorMessage(pConn));
+            PQclear(res);
+            return -1;
+        }
+
+        uint64_t one_week_ms = 7 * 24 * 60 * 60 * 1000;
+
+        char* temp = (char*)calloc(256, sizeof(char));
+        int rec_count = PQntuples(res);
+
+        printf("Urgent car count: %i\n", rec_count);
+
+        for (int i = 0; i < rec_count; i++) 
+        {
+            DBCar* pCar = new DBCar();
+            strcpy(temp, PQgetvalue(res, i, 0));
+            pCar->Id = atoi(temp);
+
+            strcpy(temp, PQgetvalue(res, i, 1));
+            pCar->IsUrgentTs = atol(temp);
+
+            // Check if the difference is greater than a week
+            if (current_time_ms - pCar->IsUrgentTs > one_week_ms) 
+            {
+                // Update the is_urgent field to 0
+                std::string update_sql = "UPDATE cars SET is_urgent = 0 WHERE id = " + std::to_string(pCar->Id) + ";";
+                PGresult* update_res = PQexec(pConn, update_sql.c_str());
+
+                if (PQresultStatus(update_res) != PGRES_COMMAND_OK) 
+                {
+                    fprintf(stderr, "UPDATE failed: %s", PQerrorMessage(pConn));
+                }
+
+                printf("Urgent car updated.\n");
+                PQclear(update_res);
+            }
+        
+            delete pCar;
+        }
+
+        // Clean up
+        free(temp);
+        PQclear(res);
+    }
+
+
+    // Refresh Top Cars
+    {
+        std::string sql = "SELECT id, on_top_ts FROM cars WHERE on_top > 0 AND on_top_ts > 0;";
+        PGresult* res = PQexec(pConn, sql.c_str());
+
+        if (PQresultStatus(res) != PGRES_TUPLES_OK) 
+        {
+            fprintf(stderr, "SELECT failed: %s", PQerrorMessage(pConn));
+            PQclear(res);
+            return -1;
+        }
+
+        char* temp = (char*)calloc(256, sizeof(char));
+        int rec_count = PQntuples(res);
+        printf("Top car count: %i\n", rec_count);
+
+        for (int i = 0; i < rec_count; i++) 
+        {
+            DBCar* pCar = new DBCar();
+            strcpy(temp, PQgetvalue(res, i, 0));
+            pCar->Id = atoi(temp);
+
+            strcpy(temp, PQgetvalue(res, i, 1));
+            pCar->OnTopTs = atol(temp);
+
+            // Update the refresh_ts field to current_time_ms
+            std::string update_sql = "UPDATE cars SET refresh_ts = " + std::to_string(current_time_ms) + " WHERE id = " + std::to_string(pCar->Id) + ";";
+            PGresult* update_res = PQexec(pConn, update_sql.c_str());
+
+            if (PQresultStatus(update_res) != PGRES_COMMAND_OK) 
+            {
+                fprintf(stderr, "UPDATE failed: %s", PQerrorMessage(pConn));
+            }
+            printf("Top car refreshed.\n");
+            PQclear(update_res);          
+            delete pCar;
+        }
+
+         // Clean up
+        free(temp);
+        PQclear(res);
+    }
+    ConnectionPool::Get()->releaseConnection(pConn);
+   /* _ParseCars(res, cars);
+
+
 
     std::vector<DBCar*> tmpCars;
 
@@ -115,7 +215,7 @@ int main()
     {
         sql = "UPDATE users SET num_golds = 10, last_gold_gift = " + std::to_string(ms) +  " WHERE id = " + std::to_string(pUser->Id) + ";";
         res = PQexec(pConn, sql.c_str());
-    }
+    }*/
 
 
     return 0;
